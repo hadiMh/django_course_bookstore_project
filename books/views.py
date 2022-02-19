5486bc357e8ffa6fb52fad302b8fa75eea15237a
MohammadHadi Hajihosseini <m.hadi.hajihosseini@gmail.com>
2022-02-19 05:21:00 +0330

    add BookListView

--- books/views.py
+++ books/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from .models import Book
+
+class BookListView(generic.ListView):
+    model = Book
+    template_name = 'books/book_list.html'
+    context_object_name = 'books'
--- config/urls.py
+++ config/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('', include('pages.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('accounts.urls')),
+    path('books/', include('books.urls')),
 ]
 
