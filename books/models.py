e5cdd4f6837d010f6eb955ca41dbb59ad3589a1f
MohammadHadi Hajihosseini <m.hadi.hajihosseini@gmail.com>
2022-02-21 02:26:36 +0330

    add user to book model who created the book object

--- books/models.py
+++ books/models.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 from django.contrib.auth import get_user_model
 
 class Book(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
     description = models.TextField()
